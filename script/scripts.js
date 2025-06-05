224f529bc0c21f41066bb8b38df4caefb12513d5
dmt48 <demeterdebru@gmail.com>
2025-06-05 14:38:16 +0300

    Update scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -1,1 +1,37 @@
-
+function openDictionary() {
+        window.location.href = '/dictionary.html'
+}
+function openEducation() {
+        window.location.href = '/education.html'
+}
+// lesson functions //
+function openLesson1() {
+        window.location.href = '/lesson/lesson1.html'
+}
+function openLesson2() {
+        window.location.href = '/lesson/lesson2.html';
+}
+function openLesson3() {
+        window.location.href = '/lesson/lesson3.html'
+}
+function openLesson4() {
+        window.location.href = '/lesson/lesson4.html';
+}
+function openLesson5() {
+        window.location.href = '/lesson/lesson5.html'
+}
+function openLesson6() {
+        window.location.href = '/lesson/lesson6.html';
+}
+function openLesson7() {
+        window.location.href = '/lesson/lesson7.html'
+}
+function openLesson8() {
+        window.location.href = '/lesson/lesson8.html';
+}
+function openLesson9() {
+        window.location.href = '/lesson/lesson9.html'
+}
+function openLesson10() {
+        window.location.href = '/lesson/lesson10.html';
+}
